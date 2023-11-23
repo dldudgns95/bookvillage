beb29704163c8202431daabc5adc9cb1f0a417eb
dldudgns95 <dldudgns95@gmail.com>
2023-11-23 16:04:26 +0900

    231123_lyh_기본세팅

---
+++ src/main/java/kr/co/bookvillage/BookvillageApplication.java
@@ -0,0 +1,13 @@
+package kr.co.bookvillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookvillageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookvillageApplication.class, args);
+	}
+
+}
