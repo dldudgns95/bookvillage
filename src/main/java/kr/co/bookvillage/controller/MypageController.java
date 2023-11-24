1d84be980a71b22cfd86651df032f258f2c32036
eylee02 <leywindy@naver.com>
2023-11-24 14:41:24 +0900

    20231124

--- src/main/java/kr/co/bookvillage/controller/MypageController.java
+++ src/main/java/kr/co/bookvillage/controller/MypageController.java
@@ -2,17 +2,27 @@ package kr.co.bookvillage.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/mypage")
 @Controller
 public class MypageController {
   
   // 서비스 객체선언
   
-  
-  @GetMapping(value="/mypage/list.do")
+  // 마이페이지 메인목록
+  @GetMapping(value="/list.do")
   public String list() {
     return "mypage/list";
   }
+  
+  // 회원정보 수정페이지 이동
+  @GetMapping(value="/edit.form")
+  public String edit(int userNo) {
+    return "mypage/edit";
+  }
+  
+  
   
   
 }
