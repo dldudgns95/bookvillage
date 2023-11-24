a939148c7652cfe3b2caca9b0205bd1476911993
gsyummy <tkfkdgo7557@gmail.com>
2023-11-24 16:15:37 +0900

    sy_231124

---
+++ src/main/java/kr/co/bookvillage/service/QnaServiceImpl.java
@@ -0,0 +1,35 @@
+package kr.co.bookvillage.service;
+
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+
+import lombok.RequiredArgsConstructor;
+
+@Transactional
+@RequiredArgsConstructor
+@Service
+public interface QnaServiceImpl extends QnaService {
+	/*
+	//private final QnaMapper qnaMapper;
+	//private final MyPageUtils myPageUtils;
+	
+	// 목록보기 + 페이징
+	@Transactional(readOnly=true)
+	@Override
+	public void loadQnaList(HttpServletRequest request, Model model) {
+		
+		// 페이지 정보 
+		Optional<String> opt = Optional.ofNullable(request.getParameter("page"));
+	    int page = Integer.parseInt(opt.orElse("1"));  // 페이지 번호가 전달되지 않았을때 1로 전달
+	    
+	    int display = 10;
+	    
+	    int total = qnaMapper.getQnaCount();  // 게시글의 개수 구하기
+	}
+}
+*/
