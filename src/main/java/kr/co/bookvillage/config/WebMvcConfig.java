43487b9987068a070a198f8702da2180c0d8c422
soosuuj <jangsj2104@naver.com>
2023-11-24 09:53:24 +0900

    20231124

--- src/main/java/kr/co/bookvillage/config/WebMvcConfig.java
+++ src/main/java/kr/co/bookvillage/config/WebMvcConfig.java
@@ -29,9 +29,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
   
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//    registry.addResourceHandler("/**")
-//      .addResourceLocations("classpath:/static/", "classpath:/templates/");
-// 
+    registry.addResourceHandler("/**")
+      .addResourceLocations("classpath:/static/", "classpath:/templates/");
+ 
   }
   
 }
