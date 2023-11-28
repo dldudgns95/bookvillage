b618c653f673404e924fa60db54a13ab089b80ef
gaeul56 <gaeul56@naver.com>
2023-11-28 15:23:55 +0900

    sql수정

--- src/main/resources/static/sql/북빌리지.sql
+++ src/main/resources/static/sql/북빌리지.sql
@@ -50,8 +50,8 @@ CREATE TABLE USER_T (
   USER_NO          NUMBER             NOT NULL,             -- 회원번호
   EMAIL            VARCHAR2(100 BYTE) NOT NULL UNIQUE,   -- 회원이메일
   PW               VARCHAR2(64 BYTE)  NOT NULL,           -- 비밀번호
-  NAME             VARCHAR2(50 BYTE)  UNIQUE,    -- 회원이름
-  MOBILE           VARCHAR2(15 BYTE),           -- 전화번호
+  NAME             VARCHAR2(50 BYTE),    -- 회원이름
+  MOBILE           VARCHAR2(15 BYTE)  UNIQUE,           -- 전화번호
   GENDER           VARCHAR2(5 BYTE),            -- 성별 (0:남자, 1:여자, 3:선택안함)
   AGREE            NUMBER,                      -- 동의 (0:필수, 1: 이벤트)
   STATE            NUMBER,                      -- 상태 (가입형태, 0:일반회원, 1:네이버간편로그인, 2:구글간편로그인)
@@ -76,7 +76,7 @@ CREATE TABLE INACTIVE_USER (
   PW                 VARCHAR2(64 BYTE)  NULL,      -- 회원비밀번호
   NAME               VARCHAR2(50 BYTE)  NULL,      -- 회원이름
   MOBILE             VARCHAR2(15 BYTE)  NULL,      -- 회원휴대전화
-  GENDER             NUMBER             NULL,      -- 회원성별(0:남자, 1:여자, 2:선택안함)
+  GENDER             VARCHAR2(5 BYTE)   NULL,      -- 회원성별(0:남자, 1:여자, 2:선택안함)
   AGREE              NUMBER             NULL,      -- 서비스동의여부(0:필수, 1:이벤트)
   STATE              NUMBER             NULL,      -- 가입형태(0:일반, 1:네이버간편로그인, 2:구글간편로그인)
   AUTH               NUMBER             NULL,      -- 회원등급(0:일반, 1:휴면, 9:관리자)
